eaaeed98a4cfd94547f3898929cd7554c82eb8c2
Felipe Alves <felipealexandre1408@gmail.com>
2024-01-09 18:21:28 -0300

    Product

---
+++ src/main/java/com/example/SpringBootTeste/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.example.SpringBootTeste.services;
+
+import com.example.SpringBootTeste.entities.Product;
+import com.example.SpringBootTeste.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findBydId(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
